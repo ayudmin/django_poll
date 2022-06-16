fcfe5586dfbe8410974260ed7123a0c469687fb6
francis556 <vikrammehra244@gmail.com>
2022-06-16 12:02:05 +0200

    mininal form

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from .models import Question
+from django.urls import reverse
 
 
 def index(request):
@@ -16,10 +18,23 @@ def detail(request, question_id):
 	}
 	return render(request, 'polls/detail.html', context)
 def results(request, question_id):
-	pass
-	# response = "You're looking at the results of question %s. "
-	# return HttpResponse(response % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	context = {
+		'question': question
+	}
+	return render(request, 'polls/results.html', context)
 
 def vote(request, question_id):
-	pass
-	# return HttpResponse("You're voting on question %s. " % question_id)
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		context = {
+			'question': question,
+			'error_message': "You didn't select a choice",
+		}
+		return render(request, 'polls/detail.html', context)
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
