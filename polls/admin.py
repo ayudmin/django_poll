831eab44ba638d01a2b7d1fc9869f163e1b76f10
francis556 <vikrammehra244@gmail.com>
2022-06-17 22:06:22 +0200

    customizing the change list

--- polls/admin.py
+++ polls/admin.py
@@ -10,9 +10,14 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
 	fieldsets = [
-	(None, {'fields': ['question_text']}),
-	('Date information', {'fields': ['pub_date']}),
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date']}),
 	]
 	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	
+
+	list_filter = ['pub_date']
+	search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)
@@ -9,9 +10,17 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 
+	@admin.display(
+		boolean=True,
+		ordering='pub_date',
+		description='Published recently'
+		
+	)
+
 	def was_published_recently(self):
 		now = timezone.now()
-		return now - datetime.timedelta(days=1) <= self.pub_date <= now
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now	
+
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
